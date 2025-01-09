9262900975cd1683ec94a73e7a63b8867653d9eb
Kanhaiya Kumar <88824128+kanhaiyakk@users.noreply.github.com>
2025-01-09 16:58:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            String input = br.readLine();
+            String[] inputArray = input.split("\\s+");
+            int a[] = new int[inputArray.length];
+
+            for (int i = 0; i < a.length; i++) a[i] = Integer.parseInt(inputArray[i]);
+
+            Solution ob = new Solution();
+            ob.sort012(a);
+
+            for (int num : a) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+            System.out.println("~");
+        }
+    }
+}
+
+
+// } Driver Code Ends
+class Solution {
+    // Function to sort an array of 0s, 1s, and 2s
+    public void sort012(int[] arr) {
+        Arrays.sort(arr);
+        
+        
+        //Inssertion sort(Tile limit exceed)
+        // for(int i=1;i<arr.length;i++){
+        //     int curr=arr[i];
+        //     int j=i-1;
+        //     while(j>=0 && curr<arr[j]){
+        //         arr[j+1]=arr[j];
+        //         j--;
+        //     }
+        //     arr[j+1]=curr;
+        // }
+    }
+}
+
+//{ Driver Code Starts.
+// } Driver Code Ends
