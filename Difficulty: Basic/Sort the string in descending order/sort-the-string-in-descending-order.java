a7321bb07d195c2930e8f3f4706c550517530603
Kanhaiya Kumar <88824128+kanhaiyakk@users.noreply.github.com>
2025-01-17 20:03:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Sort the string in descending order/sort-the-string-in-descending-order.java
+++ Difficulty: Basic/Sort the string in descending order/sort-the-string-in-descending-order.java
@@ -1,34 +1,34 @@
 //{ Driver Code Starts
-//Initial Template for Java
+// Initial Template for Java
 import java.io.*;
-import java.util.*; 
-class GFG{
-    public static void main(String args[]) throws IOException { 
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(read.readLine().trim());
-        while(t-- > 0){
+        while (t-- > 0) {
             String str = read.readLine().trim();
             Solution ob = new Solution();
             System.out.println(ob.ReverseSort(str));
-        
-System.out.println("~");
-}
+
+            System.out.println("~");
+        }
     }
 }
 // } Driver Code Ends
 
 
-//User function Template for Java
-class Solution 
-{ 
-    String ReverseSort(String str) 
-    { 
-        // code here
-       char[] ch=str.toCharArray();
-       Arrays.sort(ch);
-       String s1=new String(ch);
-       StringBuilder rev=new StringBuilder(s1);
-       rev.reverse();
-       return rev.toString();
+// User function Template for Java
+class Solution {
+    String ReverseSort(String s) {
+       StringBuilder sb=new StringBuilder();
+       
+        char[] charArray=s.toCharArray();
+        Arrays.sort(charArray);
+        for(int i=charArray.length-1;i>=0;i--){
+            sb.append(charArray[i]);
+        }
+        return sb.toString();
     }
-} 
+}
