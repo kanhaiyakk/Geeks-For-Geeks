d163df8c1cca37fb9d590e904bf87012269790c4
Kanhaiya Kumar <88824128+kanhaiyakk@users.noreply.github.com>
2025-01-18 14:02:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Ceil The Floor/ceil-the-floor.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            int x = Integer.parseInt(br.readLine());
+            String[] input = br.readLine().split(" ");
+            int[] arr = new int[input.length];
+            for (int i = 0; i < input.length; i++) {
+                arr[i] = Integer.parseInt(input[i]);
+            }
+
+            Solution ob = new Solution();
+            int[] ans = ob.getFloorAndCeil(x, arr);
+            System.out.println(ans[0] + " " + ans[1]);
+        
+System.out.println("~");
+}
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int[] getFloorAndCeil(int x, int[] arr) {
+        int floor=-1;
+        int celi=-1;
+       int left=0;
+       int right=arr.length-1;
+       Arrays.sort(arr);
+       while(left<=right){
+           int mid=left+(right-left)/2;
+           if(arr[mid]==x){
+               return new int[]{x,x};
+           }else if(arr[mid]<x){
+               floor=arr[mid];
+               left=mid+1;
+           }else{
+               celi=arr[mid];
+               right=mid-1;
+           }
+       }
+       return new int[]{floor,celi};
+    }
+}
