b243c762daf0ddbd7f2149cb684966bede4c1166
Kanhaiya Kumar <88824128+kanhaiyakk@users.noreply.github.com>
2025-02-13 17:33:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Square Root/square-root.java
+++ Difficulty: Easy/Square Root/square-root.java
@@ -23,6 +23,22 @@ this function*/
 // x: element to find square root
 class Solution {
     int floorSqrt(int n) {
-        return (int)Math.sqrt(n);
+       if(n==0 || n==1) return n;
+       int left=1,right=n,ans=0;
+       while(left<=right){
+        int mid=left+(right-left)/2;
+        long square=(long) mid*mid;
+        if(square==n){
+            return mid;
+        }else if(square <n){
+            ans=mid;
+            left=mid+1;
+        }else{
+            right=mid-1;
+        }
+       }
+       return ans;
+       
+       // return (int)Math.sqrt(n);
     }
 }
