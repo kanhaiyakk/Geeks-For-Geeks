540a2074c9348fd31a04a5c4818d3c4a997207b6
Kanhaiya Kumar <88824128+kanhaiyakk@users.noreply.github.com>
2025-06-22 19:18:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Array Duplicates/array-duplicates.java
+++ Difficulty: Easy/Array Duplicates/array-duplicates.java
@@ -1,50 +1,11 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class Geeks {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine()); // Number of test cases
-        for (int g = 0; g < t; g++) {
-            String[] str =
-                (br.readLine()).trim().split(" "); // Reading input as a string array
-            int arr[] = new int[str.length]; // Creating integer array from the input
-            for (int i = 0; i < str.length; i++) {
-                arr[i] = Integer.parseInt(str[i]);
-            }
-
-            // Getting the result from the Solution class
-            List<Integer> result = new Solution().findDuplicates(arr);
-            Collections.sort(result);
-            // Printing the result in the required format
-            if (result.isEmpty()) {
-                System.out.println("[]");
-            } else {
-                for (int i = 0; i < result.size(); i++) {
-                    if (i != 0) System.out.print(" ");
-                    System.out.print(result.get(i));
-                }
-                System.out.println();
-            }
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
     public List<Integer> findDuplicates(int[] arr) {
+        // code here
         List<Integer> list=new ArrayList<>();
-        Map<Integer,Integer> map=new HashMap<>();
-        for(int num: arr){
-            map.put(num,map.getOrDefault(num,0)+1);
-        }
-        for(int key:map.keySet()){
-            if(map.get(key)>1){
-                list.add(key);
+        Set<Integer> seen=new HashSet<>();
+        for(int num:arr){
+            if(!seen.add(num)){
+                list.add(num);
             }
         }
         return list;
