604a7e06f1be080f2ae158cbbf1119d07d0f39eb
Kanhaiya Kumar <88824128+kanhaiyakk@users.noreply.github.com>
2025-07-06 14:47:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Nuts and Bolts Problem/nuts-and-bolts-problem.java
@@ -0,0 +1,11 @@
+// User function Template for Java
+
+class Solution {
+    void matchPairs(int n, char nuts[], char bolts[]) {
+        // code here
+        if(nuts.length!= bolts.length) return;
+        
+        Arrays.sort(nuts);
+        Arrays.sort(bolts);
+    }
+}
